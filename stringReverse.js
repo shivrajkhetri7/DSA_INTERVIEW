695027b787e5e4ace432d54847136593aa5f8841
shivrajkhetri7 <shivrajkhetri7@gmail.com>
2023-06-18 20:19:04 +0530

    fix : resolved comment

--- stringReverse.js
+++ stringReverse.js
@@ -28,3 +28,35 @@ function stringRev(string) {
 }
 
 console.log(stringRev(string));
+
+/**
+ * String reverse
+ * @Query -> For example "Welcome to this Javascript Guide!" should be become "emocleW ot siht tpircsavaJ !ediuG"
+ */
+
+const string2 = "Welcome to this Javascript Guide!";
+
+function reverseString(string2) {
+  return string2.split("").reverse().join("").split(" ").reverse().join(" ");
+}
+
+console.log(reverseString(string2));
+
+function reverseWords(string2) {
+  const reverseData = [];
+  function reverseParam(word) {
+    let reverseWord = "";
+    for (let index = word.length - 1; index >= 0; index--) {
+      reverseWord += word[index];
+    }
+    return reverseWord;
+  }
+  const stringArray = string2.split(" ");
+  for (let index = 0; index < stringArray.length; index++) {
+    let word = reverseParam(stringArray[index]);
+    reverseData.push(word);
+  }
+  return reverseData.join(" ");
+}
+
+console.log("----> ", reverseWords(string2));
